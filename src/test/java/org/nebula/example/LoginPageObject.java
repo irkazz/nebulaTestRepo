27de87f52a580d92184ff54e0be44870fad66edc
irkazz <115847060+irkazz@users.noreply.github.com>
2023-05-17 23:10:02 -0700

    Update LoginPageObject.java

--- src/test/java/org/nebula/example/LoginPageObject.java
+++ src/test/java/org/nebula/example/LoginPageObject.java
@@ -2,11 +2,7 @@ package org.nebula.example;
 
 public class LoginPageObject {
     //To keep things simple, as I'm not planning to test this page, adding only getter
-<<<<<<< HEAD
     private static final String url = "https://app.nebula.io/";
-=======
-    private static final String url = "https://nebula.io/insights/";
->>>>>>> dev1
 
     public static String getBaseUrl() {
         return url;
